e03a53d0bcbba226bb2c3aff5648ff31cf6f824b
Vinícius Thiengo <viniciusthiengo@gmail.com>
2017-01-13 22:55:25 -0200

    Update

--- app/src/main/java/br/com/thiengo/laranjeirasguiacomercial/ComercioActivity.java
+++ app/src/main/java/br/com/thiengo/laranjeirasguiacomercial/ComercioActivity.java
@@ -1,5 +1,7 @@
 package br.com.thiengo.laranjeirasguiacomercial;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -13,10 +15,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import br.com.thiengo.laranjeirasguiacomercial.domain.Comercio;
 
-public class ComercioActivity extends AppCompatActivity {
+public class ComercioActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Comercio comercio;
 
     @Override
@@ -62,6 +70,18 @@ public class ComercioActivity extends AppCompatActivity {
         conteudo += "Domingo: <b>não funciona</b>";
         TextView tvHorarioFuncionamento = (TextView) findViewById(R.id.tv_horario_funcionamento);
         tvHorarioFuncionamento.setText( Html.fromHtml( conteudo ) );
+
+        conteudo = "Nunca esqueci da frase no filme \"Wall Street - O Dinheiro Nunca Dorme\" onde Josh Brolin interpretando Bretton James disse,<br><br>quando perguntado pelo jovem Jake Moore\t(interpretado por Shia LaBeouf) sobre qual era o número máximo dele (eles estavam falando de dinheiro). O personagem Bretton James, sem rodeios, respondeu: \"Meu número é ‘MAIS'\".<br><br>Pode soar um pouco soberbo, mas po%#@, foi fo$@&.";
+        TextView tvDescricao = (TextView) findViewById(R.id.tv_descricao);
+        tvDescricao.setText( Html.fromHtml( conteudo ) );
+
+        TextView tvLocalizacao = (TextView) findViewById(R.id.tv_localizacao);
+        tvLocalizacao.setText( Html.fromHtml( "<b>Endereço:</b> "+comercio.getLocalizacao() ) );
+
+
+        // MAPS
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fm_mapa);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -70,5 +90,40 @@ public class ComercioActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        //map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
+    public void intencoesContato( View view ){
+        Intent intent = null;
+
+        if( view.getId() == R.id.tv_telefone ){
+            String numero = "27997863727";
+            intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", numero, null));
+        }
+        else if( view.getId() == R.id.tv_email ){
+            String email = "thiengocalopsita@gmail.com";
+            intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",email, null));
+            intent.putExtra(Intent.EXTRA_TEXT, "Acesso direto de Laranjeiras Guia Comercial APP");
+            intent = Intent.createChooser(intent, "Enviar email");
+        }
+        else if( view.getId() == R.id.tv_site ){
+            String url = "http://www.thiengo.com.br";
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+        }
+
+        startActivity(intent);
     }
 }
